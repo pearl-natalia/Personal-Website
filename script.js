c56ce90f63da7c275a160fea203d6ac72dc7e196
Pearl <pnatalia@uwaterloo.ca>
2024-08-27 21:01:50 -0400

    New website design

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Height of project descriptions
+  var project2Description = document.getElementById("project2-description");
+  if (project2Description) {
+    var height = project2Description.offsetHeight;
+    var projectDescriptions = document.querySelectorAll(".project-description");
+    projectDescriptions.forEach(function (div) {
+      div.style.height = height + "px";
+    });
+  }
+
   const projectLink = document.getElementById("projects");
   const aboutLink = document.getElementById("about");
   const projectContainer = document.getElementById("project-container");
   const aboutContainer = document.getElementById("about-container");
 
-  // Function to show the appropriate section
+  // Show the appropriate section
   function showSection(section) {
     if (section === "projects") {
       projectContainer.style.visibility = "visible";
       projectContainer.style.opacity = "1";
       aboutContainer.style.visibility = "hidden";
       aboutContainer.style.opacity = "0";
-      window.location.hash = "#projects"; // Updates the URL with hash
+      window.location.hash = "#projects";
       updateBold(projectLink);
     } else if (section === "about") {
       projectContainer.style.visibility = "hidden";
       projectContainer.style.opacity = "0";
       aboutContainer.style.visibility = "visible";
       aboutContainer.style.opacity = "1";
-      window.location.hash = "#about"; // Updates the URL with hash
+      window.location.hash = "#about";
       updateBold(aboutLink);
     }
-    window.scrollTo(0, 0); // Ensure the page loads from the top
+    window.scrollTo(0, 0);
   }
 
-  // Function to bold the clicked menu item
+  // Bold clicked menu item
   function updateBold(clickedLink) {
     projectLink.classList.remove("bold");
     aboutLink.classList.remove("bold");
@@ -42,13 +52,33 @@ document.addEventListener("DOMContentLoaded", function () {
     showSection("about");
   });
 
-  // Handle hash change
+  // Hash changes
   window.addEventListener("hashchange", function () {
     const section = window.location.hash.substring(1);
     showSection(section);
   });
 
-  // Initial load based on hash
+  // Initial load
   const initialSection = window.location.hash.substring(1) || "projects";
   showSection(initialSection);
+
+  // Define the aspect ratio (width / height)
+  const aspectRatio = 1400 / 2000;
+  const projectElements = document.querySelectorAll(".project-image-container");
+  projectElements.forEach((element) => {
+    const width = element.getBoundingClientRect().width;
+    const newHeight = aspectRatio * width + 50;
+    element.style.height = `${newHeight}px`;
+  });
+
+  // About me section
+  const aboutContainerDiv = document.getElementById("#about-container");
+  const menu = document.getElementById("#menu");
+  const viewportWidth = window.innerWidth;
+  const menuHeight = menu.offsetHeight;
+  const viewportHeight = window.innerHeight;
+  const divHeight = viewportHeight - menuHeight;
+
+  if (viewportWidth < 768) divHeight %= 80;
+  aboutContainerDiv.style.height = `${divHeight}px`;
 });
